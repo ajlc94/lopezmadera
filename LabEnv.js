16c3f7e29162567a39461df223a780ff2c1e1b21
ajlc94 <lopeznet@live.com.mx>
2015-12-15 21:56:05 -0600

    Update LabEnv.js
    version de prueba labenv

--- LabEnv.js
+++ LabEnv.js
@@ -1,25 +1,30 @@
 function maze(h, w){
+
     var i, j, x, z, c, str="";
 	var temp;
 	var cont=0,der=0;
 	
-    for(i=0; i<h; i++){
+    for (i=0; i<w; i++){
+        str=str+'#'
+    }
+    str=str+'\n';
+    for(i=1; i<h; i++){
     	for(j=0; j<w; j++){
-		
-			if(i==0 && j==0){
-				str=str+'I'; 
-			} 
-            x=Math.floor(Math.random()*2);
-            if (x==1) str=str+'#';				
-            if (x==0) str=str+' ';
-			if(i==14 && j==14){
-				str=str+'S'; 
-			}
+            if (j==0||j==(w-1)){
+                str=str+'#';
+            }else{
+                x=Math.floor(Math.random()*4);
+                if (x==0) str=str+'#'; else str=str+' ';
+            }
         }
         str=str+'\n';
-    }		
+    }
+    for (i=0; i<w; i++){
+        str=str+'#'
+    }
+    str=str+'\n';  		
 	return str;
  }
  
-s=maze(15,15);
+s=maze(10,10);
 console.log(s);
