2c61538fb952306808f88808614c9e2dce334eb6
ajlc94 <lopeznet@live.com.mx>
2015-12-15 21:57:46 -0600

    Update LabMain.js
    version de prueba LabMain.js

--- LabMain.js
+++ LabMain.js
@@ -1,9 +1,15 @@
-var LabEnv = require("./LabEnv");
-var LabAgent = require("./LabAgent");
+var Simulation = require("./simulation");
+var LabEnv = require('./LabEnv');
+var LabAgent = require('./LabAgent');
 
-var x, y, h, w;
-console.log("\n ingrese altura del laberinto: ");
-console.log("\n ingrese ancho del laberinto: ");
+var env = new LabEnv();
+var agent = new LabAgent();
 
-var str=LabEnv.maze(h,w);
-console.log(str);
+var config = {
+	delay : 500,
+	ticks: 10
+};
+
+var sim = new Simulation(agent,env,config);
+
+sim.execute();
