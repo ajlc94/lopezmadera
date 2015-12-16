76d4fc4ac5ec18d4f265816cd0da45208f300df7
ajlc94 <lopeznet@live.com.mx>
2015-12-16 17:58:56 -0600

    Update LabEnv.js
    versión final

--- LabEnv.js
+++ LabEnv.js
@@ -1,30 +1,58 @@
-function maze(h, w){
+var LabEnv = function(estado){
+    this.estado = {
+        d=0;
+        x=0;
+        y=0;
 
+    }
+    this.acciones = ["izquierda","Derecha","Arriba","Abajo"];
+}
+
+
+LabEnv.prototype.maze=function(h,w){
+   
     var i, j, x, z, c, str="";
 	var temp;
 	var cont=0,der=0;
 	
     for (i=0; i<w; i++){
-        str=str+'#'
+        str=str+'#';
     }
     str=str+'\n';
-    for(i=1; i<h; i++){
+    for(i=1; i<(h-1); i++){
     	for(j=0; j<w; j++){
-            if (j==0||j==(w-1)){
-                str=str+'#';
+            if(i==1&&j==0){
+                str=str+'I';
             }else{
-                x=Math.floor(Math.random()*4);
-                if (x==0) str=str+'#'; else str=str+' ';
+                if(i==(h-2)&&j==(w-1)){
+                    str=str+'S';
+                }else{
+                    if(i==1&&j==1){
+                        str=str+' ';
+                    }else{
+                        if(i==(h-2)&&j==(w-2)){
+                            str=str+' ';
+                        }else{
+                            if (j==0||j==(w-1)){
+                                str=str+'#';
+                            }else{
+                                x=Math.floor(Math.random()*5);
+                                if (x==0) {str=str+'#';} else {str=str+' ';}
+                            }
+                        }
+                    }
+                }
             }
         }
         str=str+'\n';
     }
     for (i=0; i<w; i++){
-        str=str+'#'
+        str=str+'#';
     }
-    str=str+'\n';  		
+    str=str+'\n';
+
 	return str;
  }
  
-s=maze(10,10);
+s=maze(20,40);
 console.log(s);
